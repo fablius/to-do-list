b7131d65dace37b27abe9562d914d6dd87a439bf
W45 <warrengs51@gmail.com>
2024-12-17 22:35:38 +0800

    Add new features
    Added new lines of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Title from "./components/Title";
 import AddTodo from "./components/AddTodo";
 import Todo from "./components/Todo";
@@ -16,6 +16,27 @@ import "./App.css";
 function App() {
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
   return (
     <div className="App">
       <div>
@@ -24,11 +45,18 @@ function App() {
       <div>
         <AddTodo />
       </div>
-      <div>
-        <Todo />
+      <div className="todo_container">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        ))}
       </div>
     </div>
   );
 }
-
 export default App;
